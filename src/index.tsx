a2023e6a2f92be47b5d01c3c5a31fbc0b2ae6e74
Renan Loureiro <renanpl47@gmail.com>
2021-11-22 09:34:12 -0300

    created fake api

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react"
+import ReactDOM from "react-dom"
+import { createServer } from "miragejs"
+import { App } from "./App"
 
-import { App } from './App';
+createServer({
+  routes() {
+    this.namespace = "api"
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Transactions one",
+          amount: 400,
+          category: "Food",
+          type: "deposit",
+          created_at: new Date(),
+        },
+      ]
+    })
+  },
+})
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+)
