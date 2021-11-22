e00c138546998fd3895fe4489c95d794af73c1d3
Renan Loureiro <renanpl47@gmail.com>
2021-11-22 12:58:08 -0300

    bottuns styleds

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,9 @@
 import Modal from "react-modal"
+import { Container, TransactionTypeContainer } from "./styles"
 
+import incomeIMG from "../../assets/income.svg"
+import outcomeIMG from "../../assets/outcome.svg"
+import closeIMG from "../../assets/close.svg"
 interface NewTransactionModalProps {
   isOpen: boolean
   onRequestClose: () => void
@@ -10,8 +14,39 @@ export const NewTransactionModal = ({
   onRequestClose,
 }: NewTransactionModalProps) => {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Nova Transação</h2>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeIMG} alt="Fechar modal" />
+      </button>
+
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input placeholder="Título" />
+        <input type="number" placeholder="Valor" />
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeIMG} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button type="button">
+            <img src={outcomeIMG} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+        <input placeholder="Categoria" />
+
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   )
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -54,4 +54,41 @@ export const GlobalStyle = createGlobalStyle`
     opacity: 0.6;
     cursor: not-allowed;
   }
+
+  .react-modal-overlay{
+    background:rgba(0,0,0,0.5);
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .react-modal-content{
+    width: 100%;
+    max-width: 576px;
+
+    background: var(--background);
+    padding: 3rem;
+    border-radius: 0.25rem;
+    position: relative;
+  }
+
+  .react-modal-close{
+    position: absolute;
+    border: 0;
+    top: 1.5rem;
+    right: 1.5rem;
+    background: transparent;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.7);
+    }
+  }
 `
